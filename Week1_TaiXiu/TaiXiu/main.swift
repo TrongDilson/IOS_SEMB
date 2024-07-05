1d6c06e31e82bd21b4c036c323a7a66c76d5380d
TrongDilson <108319460+TrongDilson@users.noreply.github.com>
2024-07-05 11:43:03 +0700

    Add files via upload

--- Week1_TaiXiu/TaiXiu/main.swift
+++ Week1_TaiXiu/TaiXiu/main.swift
@@ -13,6 +13,7 @@ var isBig: Bool = false
 var house = 1000
 var player = 100
 
+// Function to shuffle dice
 func randomDice (givenNumber:Int)-> Array<Int>
 {
     var chosenSample = [0, 0, 0]
@@ -24,6 +25,7 @@ func randomDice (givenNumber:Int)-> Array<Int>
 }
 //print(randomDice(givenNumber: diceNumbers))
 
+// Function for players to decide continue to play or not
 func playerChoice()
 {
     var isWrong: Bool = false
@@ -48,6 +50,7 @@ func playerChoice()
 //playerChoice()
 //print(isContinue)
 
+//Function for player to decide the bet whether big or small
 func playerBet()
 {
     var isWrong: Bool = false
@@ -72,6 +75,7 @@ func playerBet()
 //playerBet()
 //print(isBig)
 
+// Function to sum up total rolled dice numbers
 func diceSum (rolledNumbers:Array<Int>)-> Int
 {
     var sum = 0
@@ -82,6 +86,7 @@ func diceSum (rolledNumbers:Array<Int>)-> Int
     return sum
 }
 
+// Function to read bet number from player
 func readNum()->Int
 {
     var isWrong: Bool = false
@@ -89,7 +94,7 @@ func readNum()->Int
     while(isWrong == false)
     {
         print("How much you want to bet?")
-        var betNum = Int(readLine()!)
+        let betNum = Int(readLine()!)
         if betNum != nil
         {
             isWrong = true
@@ -102,6 +107,7 @@ func readNum()->Int
     return rightNum
 }
 
+//Main Function to play Tai Xiu
 func diceGame (houseCost: Int, playerFund: Int)
 {
     print("Try your luck to win all money of the house!")
@@ -113,26 +119,90 @@ func diceGame (houseCost: Int, playerFund: Int)
     while(isContinue == true)
     {
         round += 1
-        print("The house has $\(houseCost)")
-        print("The player has $\(playerFund)")
+        print("The house has $\(currHouse)")
+        print("The player has $\(currPlayer)")
         print("Round \(round):")
+        var betNum = 0
         
-        
-        var betNum = readNum()
-        
-        print("You have bet $\(betNum)")
-        playerBet()
-        
-        var rolledNumbers = randomDice(givenNumber: diceNumbers)
-        print("The dices are: \(rolledNumbers[0]) \(rolledNumbers[1]) \(rolledNumbers[2])")
-        var sum = diceSum(rolledNumbers: rolledNumbers)
-        print("The sum of 3 dices are \(sum)")
-        if(isBig == true)
+        if (currPlayer == 0)
         {
-            
+            print("You are out of money! Good Bye!")
+            isContinue = false
+        } else if (currHouse == 0)
+        {
+            print("You have won a house! Congratualtion!")
+            isContinue = false
+        }else if (round > 1)
+        {
+            playerChoice()
         }
-        isContinue = false
+        
+        if(isContinue == true)
+        {
+            isEnough = false
+            while(isEnough == false)
+            {
+                betNum = readNum()
+                if(betNum <= currPlayer)
+                {
+                    isEnough = true
+                } else
+                {
+                    print("You have bet larger than the amount you have. Please bet again!")
+                }
+            }
+            
+            print("You have bet $\(betNum)")
+            playerBet()
+            
+            let rolledNumbers = randomDice(givenNumber: diceNumbers)
+            print("The dices are: \(rolledNumbers[0]) \(rolledNumbers[1]) \(rolledNumbers[2])")
+            
+            if(rolledNumbers[0] == rolledNumbers[1] && rolledNumbers[0] == rolledNumbers[2])
+            {
+                print("You lost \(betNum) because 3 rolled numbers are equal")
+                currPlayer -= betNum
+                currHouse += betNum
+            } else
+            {
+                let sum = diceSum(rolledNumbers: rolledNumbers)
+                print("The sum of 3 dices are \(sum)")
+                
+                if(isBig == true)
+                {
+                    if sum >= 11 && sum <= 17
+                    {
+                        print("You won \(betNum)")
+                        currPlayer += betNum
+                        currHouse -= betNum
+                    } else
+                    {
+                        print("You lost \(betNum)")
+                        currPlayer -= betNum
+                        currHouse += betNum
+                    }
+                } else
+                {
+                    if sum >= 4 && sum <= 10
+                    {
+                        print("You won \(betNum)")
+                        currPlayer += betNum
+                        currHouse -= betNum
+                    } else
+                    {
+                        print("You lost \(betNum)")
+                        currPlayer -= betNum
+                        currHouse += betNum
+                    }
+                }
+            }
+        } else
+        {
+            isContinue = false
+            print("Thank you for playing")
+        }
     }
 }
 
+//Main
 diceGame(houseCost: house, playerFund: player)
